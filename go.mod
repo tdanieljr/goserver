2a58f7745221e205947eeac715e03b43f5934c48
Timothy Daniel <tdaniel@hey.com>
2025-11-27 09:14:39 -0600

    more progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tdanieljr/goserver
 
 go 1.25.0
+
+require golang.org/x/text v0.31.0
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 type apiConfig struct {
@@ -23,7 +29,14 @@ func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 func (cfg *apiConfig) countHits(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	w.Write(fmt.Appendf(nil, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+`, cfg.fileserverHits.Load()))
+
 }
 
 func handlerHeathz(w http.ResponseWriter, r *http.Request) {
@@ -31,16 +44,53 @@ func handlerHeathz(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		C string `json:"body"`
+	}
+	type valid struct {
+		CleanedChirp string `json:"cleaned_body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	c := chirp{}
+	err := decoder.Decode(&c)
+	if err != nil {
+		log.Printf("Error decoding chirp: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	bad_words := []string{"kerfuffle", "sharbert", "fornax"}
+	if len(c.C) <= 140 {
+		w.WriteHeader(200)
+		for _, v := range bad_words {
+			c.C = strings.ReplaceAll(c.C, v, "****")
+			c.C = strings.ReplaceAll(c.C, cases.Title(language.Und).String(v), "****")
+		}
+		resp := valid{CleanedChirp: c.C}
+
+		dat, err := json.Marshal(resp)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(dat)
+	} else {
+		w.WriteHeader(400)
+	}
+
+}
 
 func main() {
 	c := apiConfig{fileserverHits: atomic.Int32{}}
 	m := http.NewServeMux()
 
-	m.Handle("/app", c.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	m.Handle("/app/", c.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	m.HandleFunc("GET /healthz", handlerHeathz)
-	m.HandleFunc("GET /metrics", c.countHits)
-	m.HandleFunc("POST /reset", c.resetHits)
+	m.HandleFunc("GET /api/healthz", handlerHeathz)
+	m.HandleFunc("GET /admin/metrics", c.countHits)
+	m.HandleFunc("POST /admin/reset", c.resetHits)
+	m.HandleFunc("POST /api/validate_chirp", validateChirp)
 	s := http.Server{Handler: m, Addr: ":8080"}
 
 	s.ListenAndServe()
