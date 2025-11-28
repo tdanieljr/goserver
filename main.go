74fe620b5e7b28599e782bcfd2765c35138fa308
Timothy Daniel <tdaniel@hey.com>
2025-11-28 09:55:22 -0600

    updated reset to deal with user table, adding in making chirps

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -20,7 +22,8 @@ import (
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-	dbQueries      *database.Queries
+	db             *database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -32,6 +35,12 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Add(-cfg.fileserverHits.Load())
+	if cfg.platform != "dev" {
+		w.WriteHeader(403)
+		w.Write([]byte("Forbidden"))
+		return
+	}
+	cfg.db.Reset(r.Context())
 }
 func (cfg *apiConfig) countHits(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -51,15 +60,21 @@ func handlerHeathz(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
-func validateChirp(w http.ResponseWriter, r *http.Request) {
-	type chirp struct {
-		C string `json:"body"`
+func (cfg *apiConfig) Chirp(w http.ResponseWriter, r *http.Request) {
+	type apiChirpResp struct {
+		Id        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
 	}
-	type valid struct {
-		CleanedChirp string `json:"cleaned_body"`
+	type apiChirp struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
 	}
+
 	decoder := json.NewDecoder(r.Body)
-	c := chirp{}
+	c := apiChirp{}
 	err := decoder.Decode(&c)
 	if err != nil {
 		log.Printf("Error decoding chirp: %s", err)
@@ -67,13 +82,27 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	bad_words := []string{"kerfuffle", "sharbert", "fornax"}
-	if len(c.C) <= 140 {
-		w.WriteHeader(200)
+	if len(c.Body) <= 140 {
 		for _, v := range bad_words {
-			c.C = strings.ReplaceAll(c.C, v, "****")
-			c.C = strings.ReplaceAll(c.C, cases.Title(language.Und).String(v), "****")
+			c.Body = strings.ReplaceAll(c.Body, v, "****")
+			c.Body = strings.ReplaceAll(c.Body, cases.Title(language.Und).String(v), "****")
 		}
-		resp := valid{CleanedChirp: c.C}
+
+		tmp := database.CreateChirpParams{Body: c.Body, UserID: c.UserID}
+		chirp, err := cfg.db.CreateChirp(r.Context(), tmp)
+		if err != nil {
+			log.Printf("Error creating chirp: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		resp := apiChirpResp{
+			Id:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		}
 
 		dat, err := json.Marshal(resp)
 		if err != nil {
@@ -81,22 +110,68 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(500)
 			return
 		}
+
+		w.WriteHeader(201)
 		w.Write(dat)
+
 	} else {
 		w.WriteHeader(400)
 	}
+}
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type User struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding user: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	apiUser := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+
+	dat, err := json.Marshal(apiUser)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(201)
+	w.Write(dat)
 
 }
 
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		panic(err)
 	}
 	dbQueries := database.New(db)
-	c := apiConfig{fileserverHits: atomic.Int32{}, dbQueries: dbQueries}
+	c := apiConfig{fileserverHits: atomic.Int32{}, db: dbQueries, platform: platform}
 	m := http.NewServeMux()
 
 	m.Handle("/app/", c.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
@@ -104,7 +179,8 @@ func main() {
 	m.HandleFunc("GET /api/healthz", handlerHeathz)
 	m.HandleFunc("GET /admin/metrics", c.countHits)
 	m.HandleFunc("POST /admin/reset", c.resetHits)
-	m.HandleFunc("POST /api/validate_chirp", validateChirp)
+	m.HandleFunc("POST /api/chirps", c.Chirp)
+	m.HandleFunc("POST /api/users", c.createUser)
 	s := http.Server{Handler: m, Addr: ":8080"}
 
 	s.ListenAndServe()
